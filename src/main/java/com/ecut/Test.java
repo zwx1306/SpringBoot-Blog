4d50ad6004cc384244ba3102a54a81bf2cc8fb61
root <1306144500@qq.com>
2020-05-15 21:10:10 +0800

    springboot整合的博客项目

--- src/main/java/com/ecut/Test.java
+++ src/main/java/com/ecut/Test.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test {
     public static void main(String[] args) {
-        System.out.println("z");
         SpringApplication.run(Test.class, args);
     }
 
